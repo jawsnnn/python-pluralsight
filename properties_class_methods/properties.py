9f2b5e93353f60b807a36a7130b3480192e8c8c5
jawsnnn <arpanmalviya@gmail.com>
2017-10-12 07:50:25 -0400

    Properties with inheritance

--- properties_class_methods/properties.py
+++ properties_class_methods/properties.py
@@ -4,6 +4,11 @@ class Shipping:
 
     next_serial = 1337
 
+    # Assume that all shipping containers have the same width and height
+
+    HEIGHT_FT = 10.0
+    WIDTH_FT = 5.0
+
     @classmethod
     def _get_next_serial_num(cls):
         Shipping.next_serial += 1
@@ -14,19 +19,24 @@ class Shipping:
         return owner_code+'U'+str(serial).zfill(6)
 
     @classmethod
-    def create_empty(cls, owner_code, *args, **kwargs):
-        return cls(owner_code, contents = None, *args, **kwargs)
+    def create_empty(cls, owner_code, length, *args, **kwargs):
+        return cls(owner_code = owner_code, contents = None, length = length, *args, **kwargs)
 
     @classmethod
-    def create_with_list(cls, owner_code, items, *args, **kwargs):
-        return cls(owner_code, contents = list(items), *args, **kwargs)
+    def create_with_list(cls, owner_code, items, length, *args, **kwargs):
+        return cls(owner_code = owner_code, contents = list(items), length = length, *args, **kwargs)
+
+    @property
+    def volume_ft(self):
+        return Shipping.HEIGHT_FT * Shipping.WIDTH_FT * self.length
 
     # This can be easily overwritten
     def non_static(self):
         print("This is a shipping container")
 
-    def __init__(self, owner_code, contents):
+    def __init__(self, owner_code, contents, length):
         self.owner_code = owner_code
+        self.length = length
         self.contents = contents
         # Polymorphism does not work with static methods
         self.bic = self._get_bic_code(owner_code = self.owner_code,
@@ -35,6 +45,9 @@ class Shipping:
 class RefrigeratedShipping(Shipping):
 
     MAX_CELSIUS = 4.0
+
+    # This says that the volume of refrigeration equipment is 100
+    REFRIGERATED_EQUIP_VOLUME = 100.0
 
     @staticmethod
     def _f_to_c(fahrenheit):
@@ -56,8 +69,8 @@ class RefrigeratedShipping(Shipping):
     def _get_bic_code(owner_code, serial):
         return owner_code+'R'+str(serial).zfill(6) 
 
-    def __init__(self, owner_code, contents, celsius):
-        super().__init__(owner_code, contents)
+    def __init__(self, owner_code, contents, celsius, length):
+        super().__init__(owner_code, contents, length)
         self.celsius = celsius
 
     @property
@@ -82,3 +95,29 @@ class RefrigeratedShipping(Shipping):
     @fahrenheit.setter
     def fahrenheit(self, value):
         self.celsius = RefrigeratedShipping._f_to_c(value)
+
+    @property
+    def volume_ft(self):
+        return super().volume_ft - RefrigeratedShipping.REFRIGERATED_EQUIP_VOLUME 
+
+
+
+class HeatedShipping(RefrigeratedShipping):
+
+    MIN_CELSIUS = -20.0
+    
+    # It is difficult overriding setters. Research why. Here we wanted to:
+    # Create a new celsius setter to check for min. temp as well as max. temp
+    # defined in refrig class. But the setter cannot have @celsius.setter because
+    # celsius is not in scope. Hence need to qualify with class name (how does that work without object? Are properties class methods?
+    #   No. they are Python objects - property() which have fset and fget funcs that get assigned to decorated getters, setters
+    # In a way, calling RefrigeratedShipping.celsius.setter creates a property object and wraps its fset function around this celsius setter
+
+    # Check heated shipping condition, then if ok, call parent's setter function
+    # But we cannot simply call super().celsius. Doesn't work. Not explained why. Look into why? Have to explicitly call fset in parent property object
+
+    @RefrigeratedShipping.celsius.setter
+    def celsius(self, value):
+        if value < HeatedShipping.MIN_CELSIUS:
+            raise ValueError("Too cold")
+        return  RefrigeratedShipping.celsius.fset(self, value)
